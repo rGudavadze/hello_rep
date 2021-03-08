074f36208c48c2b4cd5759335fac8c05018cd45e
rGudavadze <roma.gudavadze@gmail.com>
2021-03-08 16:25:56 +0400

    add flask app

---
+++ flask_prep.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("hello.html", name=request.args.get("name", "world"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
