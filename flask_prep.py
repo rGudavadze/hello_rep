361be71f1a4e36d5036b7e4200029ee7cc2e357d
rGudavadze <roma.gudavadze@gmail.com>
2021-03-08 22:42:02 +0400

    register

--- flask_prep.py
+++ flask_prep.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template("hello.html", name=request.args.get("name", "world"))
+REGISTRANTS = {}
+
+SPORTS = [
+    "Dodgeball",
+    "Football",
+    "Soccer",
+    "Valleyball",
+    "Frisbee",
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing Name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing Sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid Sport")
+
+    REGISTRANTS[name] = sport
+
+    return redirect("/registrants")
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
